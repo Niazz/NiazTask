38f0ff59e26fe05755d3637fe33694c8deb7a7a7
Niaz <mhd_niaz@hotmail.co.uk>
2023-12-01 10:36:33 +0000

    Changes to terraform provider config

--- provider.tf
+++ provider.tf
@@ -7,3 +7,21 @@ terraform {
   }
 
 }
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "niazOrg"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "niaz"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+  access_key = "AKIAZGCARXIXXIIHHSAA"
+  secret_key = "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"
+}
